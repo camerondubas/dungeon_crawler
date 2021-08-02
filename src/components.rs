ad32c319a3e478aec2f595a95ff2f39ae4b86754
Cameron Dubas <camerondubas@gmail.com>
2021-08-02 16:14:57 -0700

    Add Victory Condition

--- src/components.rs
+++ src/components.rs
@@ -38,3 +38,8 @@ pub struct Name(pub String);
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct ChasingPlayer;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct Item;
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct AmuletOfYala;
