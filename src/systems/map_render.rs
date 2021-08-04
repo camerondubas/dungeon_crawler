624a744797e88455441fb14c798defe5cd2e4eea
Cameron Dubas <camerondubas@gmail.com>
2021-08-03 20:07:40 -0700

    Implement Field Of Views

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -1,20 +1,37 @@
 use crate::prelude::*;
 
 #[system]
-pub fn map_render(#[resource] map: &Map, #[resource] camera: &Camera) {
+#[read_component(FieldOfView)]
+#[read_component(Player)]
+pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Camera) {
+    let mut fov = <&FieldOfView>::query().filter(component::<Player>());
+
     let mut draw_batch = DrawBatch::new();
     draw_batch.target(0);
+
+    let player_fov = fov.iter(ecs).nth(0).unwrap();
+
     for y in camera.top_y..=camera.bottom_y {
         for x in camera.left_x..camera.right_x {
             let point = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
-            if map.in_bounds(point) {
+            let idx = map_idx(x, y);
+
+            if map.in_bounds(point)
+                && (player_fov.visible_tiles.contains(&point) | map.revealed_tiles[idx])
+            {
+                let tint = if player_fov.visible_tiles.contains(&point) {
+                    WHITE
+                } else {
+                    LIGHTBLUE
+                };
+
                 let idx = map_idx(x, y);
                 let glyph = match map.tiles[idx] {
                     TileType::Floor => to_cp437('.'),
                     TileType::Wall => to_cp437('#'),
                 };
-                draw_batch.set(point - offset, ColorPair::new(WHITE, BLACK), glyph);
+                draw_batch.set(point - offset, ColorPair::new(tint, BLACK), glyph);
             }
         }
     }
